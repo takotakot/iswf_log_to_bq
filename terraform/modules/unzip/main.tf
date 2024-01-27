f68f2f5c3d805ab16403ff6e3b0197f7c554492a
takotakot <takotakot@users.noreply.github.com>
2024-01-27 20:25:37 +0900

    feat: Ignore docker_repository changes

--- terraform/modules/unzip/main.tf
+++ terraform/modules/unzip/main.tf
@@ -107,6 +107,7 @@ resource "google_cloudfunctions2_function" "default" {
       service_config[0].service,
       service_config[0].service_account_email,
       build_config[0].entry_point,
+      build_config[0].docker_repository,
     ]
   }
 
